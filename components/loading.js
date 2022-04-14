3337fb0246ab0a815c6ec695bcbbff64b479e708
RuhaimHakeem <ruhaim860@gmail.com>
2022-04-14 15:25:15 +0530

    Changed Logo

--- components/loading.js
+++ components/loading.js
@@ -4,12 +4,6 @@ const Loading = () => {
   return (
     <center style={{ display: "grid", placeItems: "center", height: "100vh" }}>
       <div>
-        <img
-          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png"
-          alt=""
-          style={{ marginBottom: 10 }}
-          height={170}
-        />
         <Circle color="#3CBC28" size={60} />
       </div>
     </center>
